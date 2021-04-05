b567ae2d2c5ef5d650422b288235a1bf9d0c121c
j7olivar <j7olivar@ucsd.edu>
2021-04-04 17:59:25 -0700

    finished adding favorites

--- classes_masterclass/src/courseData.js
+++ classes_masterclass/src/courseData.js
@@ -3,31 +3,26 @@ import './App.css'
 import {Button} from '@material-ui/core';
 
 function CourseData (props){
+
   const [faves, setFaves] = useState([]);
   const [buttonColor, setButtonColor] = useState('primary');
   const [buttonText, setButtonText] = useState('Save');
-  //const [buttonVar,setButtonVar] = useState('outlined');
 
   //function to save courses to faves
   const saveOnClick= (id) => {
-    //want to change the filling of button
-    //want to change the text inside button 
-    console.log("before: "+faves);
-
+    //remove item from favorites list
     if(faves.includes(id)){
       setButtonColor('primary');
       setButtonText('Save');
-      //setButtonVar('outlined');
       setFaves(  faves.filter(item => item !== id));
+      
     }
+    //add item to favorites list
     else{
-      console.log('here');
       setButtonColor('secondary');
       setButtonText('Saved');
-      //setButtonVar('contained');
-      setFaves(faves.concat(id));
+      setFaves(faves.concat([id]));
     }
-   
     console.log(faves);
   }
 
@@ -39,6 +34,7 @@ function CourseData (props){
           <img src={data.instructor_image_url} className="ClassItem-image"/>
           <h2 className="instructorName">{data.instructor_name}</h2>
           <h3 className="courseTitle">{data.title}</h3>
+          <p className= "courseTitle">{data.id}</p>
           <p className="courseRating">Rating: {data.rating} out of 5</p>
          
           <Button 
